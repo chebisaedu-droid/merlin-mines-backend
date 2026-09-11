3dddf5f87873aff8de587ce908b8fdb01bfcedc3
chebisaedu-droid <chebisaedu@gmail.com>
2026-09-11 19:19:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ const createStkPayload = (phone, playerLabel) => {
   
   // Dynamically hash the signature using the precise current timestamp
   const livePassword = Buffer.from(
-    `${ShortCode}${LIVE_PASSKEY}${liveTimestamp}`
+    `${ShortCode}${MPESA_PASSKEY}${liveTimestamp}`
   ).toString('base64');
 
   return {
